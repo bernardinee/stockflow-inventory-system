dcf13851c1b242cffe815c3f985fe836eb2c0eb8
Kwabena Adusei <okrahkwabenaebo@gmail.com>
2026-02-02 09:07:42 +0000

    Update API URL to use environment variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base API URL - change this to your backend URL
-const API_URL = 'https://stockflow-inventory-system-nvqv.onrender.com/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 // Create axios instance with default config
 const api = axios.create({
